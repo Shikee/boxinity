fb251d4ed60b98fea374b26b84f02452b5099a40
Shik <shiny742000@hotmail.com>
2018-05-27 16:10:52 +0900

    회원가입

--- boxinity/urls.py
+++ boxinity/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from chatting import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('/',views.index),
-    path('accounts/', include('django.contrib.auth.urls')),
+	path('',views.index),
+    path('accounts', include('django.contrib.auth.urls')),
+    path('signup', views.signup, name='signup'),
 
 ]
